3c038e1641f8328e93e0c16cb4e023389ea1269b
jungi Hong <hjg020531@naver.com>
2026-03-22 16:28:04 +0900

    refactor: 사용되지 않는 import문 제거

--- src/main/java/com/surfit/backend/TestController.java
+++ src/main/java/com/surfit/backend/TestController.java
@@ -1,7 +1,6 @@
 package com.surfit.backend;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
